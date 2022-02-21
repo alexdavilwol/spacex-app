32602edc74431c4734088a2fe7b1c7461cb5c034
Alex Davila-Wollheim <alexdavila-wollheim@Alexs-iMac.attlocal.net>
2022-02-21 07:19:28 -0500

    General Page Outline

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+
+const API_URL = "https://api.spaceXdata.com/v3/launches?limit=100";
 
 function App() {
+//function fetchMissionsHandler() {
+  //fetch(API_URL).then;
+//}
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <h2>Spacex Launch Programs</h2>
+      <div>
+        <h2>Filters</h2>
+        <section>
+          <h3>Launch Year</h3>
+          <button>year button</button>
+        </section>
+        <section>
+          <h2>Successful Launch</h2>
+          <button>true or false button</button>
+        </section>
+        <section>
+          <h2>Successful Landing</h2>
+          <button>true or false button</button>
+        </section>
+      </div>
+
+      <div>Missions</div>
+    </React.Fragment>
   );
 }
 
